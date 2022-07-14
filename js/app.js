ef862a2821fc7f5e279a1fc3a4e450b2e1df4a2c
gi-manzano <manzanogisele85@gmail.com>
2022-07-15 00:31:19 +0100

    js

---
+++ js/app.js
@@ -0,0 +1,64 @@
+Vue.component('MealListado',{
+    props: {
+      estilo: Object,
+      dato: Array,
+      platos: Array
+    },
+
+    template: //html
+
+    `
+  <div>
+    <span v-bind:style= "estilo">
+      {{ dato }} 
+    </span>
+  </div>
+
+
+      `
+      
+});
+
+
+ new Vue( {
+    el:'#app',
+    data:{
+
+        platos: [
+
+            {
+              id: 1,
+              meal: "Fideos a la boloñesa ",
+              precio: 350,
+              stock: 2
+            },
+
+          {
+            id: 2,
+            meal: "Pizza a la napolitana",
+            precio: 400,
+            stock: 4
+          },
+
+          {
+            id: 3,
+            meal: "Milanesas con pure",
+            precio: 300,
+            stock: 5
+          },
+
+            
+          {
+            id: 4,
+            meal: "Ensalada caprese",
+            precio: 500,
+            stock: 7
+          },
+      
+            
+        ],
+
+        estilo: {'background' : "pink"}
+    },
+   
+});

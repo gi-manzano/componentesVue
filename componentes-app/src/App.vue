3e9daa53ea2d09f5b74307344664208a6a37c015
gi-manzano <manzanogisele85@gmail.com>
2022-07-17 21:15:18 +0100

    vue/cli

--- componentes-app/src/App.vue
+++ componentes-app/src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+        <table class="table">
+            <thead class="head">
+              <tr>
+                <td>Meal</td>
+                <td >Precio</td>
+                <td>Stock</td>
+              </tr>
+            </thead>
+            <tbody class="tbody">
+              <tr v-for="(item) in platos" :key="item.id">
+                <td> <Meal-listado :estilo="estilo" :platos="item.meal"></Meal-listado></td>
+                <td>$<Meal-listado :precio="item.precio"></Meal-listado></td>
+                <td> <Meal-listado :stock="item.stock"></Meal-listado></td>
+              </tr>
+            </tbody>
+        </table>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MealListado from '../src/components/MealListado.vue'
+
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    MealListado
+  },
+  data () {
+     return {
+       platos: [
+
+            {
+              id: 1,
+              meal: "Fideos a la boloñesa ",
+              precio: 350,
+              stock: 2
+            },
+
+          {
+            id: 2,
+            meal: "Pizza a la napolitana",
+            precio: 400,
+            stock: 4
+          },
+
+          {
+            id: 3,
+            meal: "Milanesas con pure",
+            precio: 300,
+            stock: 5
+          },
+
+
+          {
+            id: 4,
+            meal: "Ensalada caprese",
+            precio: 500,
+            stock: 7
+          },
+
+
+        ],
+
+        estilo: {'background' : "pink"}
+     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.head {
+    background-color: yellowgreen;
+    padding: auto;
+    width: 14px;
+}
+
+.tbody {
+    background-color: azure;
 }
 </style>
---
+++ componentes-app/src/components/MealListado.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+<div>
+    <span v-bind:style= "estilo">
+      {{ stock }}
+      {{ platos }}
+      {{ precio }}
+    </span>
+  </div>
+
+
+</template>
+
+<script >
+
+  export default  {
+    name: 'Meal-listado',
+     props: {
+      estilo: Object,
+      stock: Number,
+      platos: String,
+      precio: Number
+    },
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+
+</style>
